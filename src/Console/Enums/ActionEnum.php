c60d46272fba441b7c248575ac08ab62b0fa5713
Aleksey Yakushev <r3bzya@gmail.com>
2025-04-08 19:58:40 +0300

    feat: Extend ActionEnum with additional aliases

--- src/Console/Enums/ActionEnum.php
+++ src/Console/Enums/ActionEnum.php
@@ -14,10 +14,13 @@ enum ActionEnum
             self::Create => [
                 'create',
                 'make',
+                'store',
             ],
             self::Update => [
                 'update',
                 'edit',
+                'put',
+                'patch',
             ],
             self::Destroy => [
                 'destroy',
